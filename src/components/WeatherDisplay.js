266736c90232389051eda37b7c58c3ab09409c1f
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-28 17:46:35 +0530

    fifth commit

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -3,8 +3,8 @@ import React from 'react'
 const WeatherDisplay = (props) => {
     return (
       <div>
-         <p>Temperature: <span style={{color: Number(props.currTemp) > 20 ? "red" : "blue"}}>{props.currTemp}</span> </p>
-         <p>Conditions: {props.currCond}</p> 
+         <p>Temperature: <span style={{color: Number(props.currTemp) > 20 ? "red" : "blue"}}>{props.currTemp}</span></p>
+         <p>Conditions:{props.currCond}</p> 
       </div>
     )
   }
