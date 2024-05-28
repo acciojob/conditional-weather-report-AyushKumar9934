b42648123b8fe104866f0b0af56ac35309c80204
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-28 17:36:45 +0530

    fourth commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,17 @@ import './../styles/App.css';
 import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
-  const [currTemp,setCurrTemp]=useState("");
-  const [currCond,setCurrCond]=useState("");
+  const [currTemp, setCurrTemp] = useState("25");
+  const [currCond, setCurrCond] = useState("Sunny");
+  
   return (
     <div>
         {/* Do not remove the main div */}
-       <input type="num" onChange={(e)=>setCurrTemp(e.target.value)}  placeholder="Temp"/>
-       <input type="text" onChange={(e)=>setCurrCond(e.target.value)} placeholder="condition"/>
+       <input type="num" onChange={(e)=>setCurrTemp(e.target.value)} value={currTemp} placeholder="Temp"/>
+       <input type="text" onChange={(e)=>setCurrCond(e.target.value)} value={currCond} placeholder="condition"/>
        <WeatherDisplay currTemp={currTemp} currCond={currCond} />
     </div>
   )
 }
 
-export default App
+export default App;
